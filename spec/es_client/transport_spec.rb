9196e9188513341b5a70da26d4c8b7f8bdb049e4
Alex Leschenko <leschenko.al@gmail.com>
2015-03-04 15:33:26 +0200

    Fix specs

--- spec/es_client/transport_spec.rb
+++ spec/es_client/transport_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe EsClient::Transport do
+describe EsClient::Client do
   describe 'request' do
     it 'initialize excon' do
       expect(Excon).to receive(:new).with('http://example.com', {persistent: true}).and_return(double(:http).as_null_object)
-      EsClient::Transport.new('http://example.com', {persistent: true}).get('/example')
+      EsClient::Client.new('http://example.com', {persistent: true}).get('/example')
     end
 
     context 'success' do
@@ -13,19 +13,19 @@ describe EsClient::Transport do
       end
 
       it 'make request' do
-        transport = EsClient::Transport.new('http://example.com', {})
+        transport = EsClient::Client.new('http://example.com', {})
         expect(transport.http).to receive(:request).with(hash_including(method: :options)).and_return(double(:response).as_null_object)
         transport.request(method: :options, path: '/example', mock: true)
       end
 
       it 'make get request' do
-        transport = EsClient::Transport.new('http://example.com', {})
+        transport = EsClient::Client.new('http://example.com', {})
         expect(transport.http).to receive(:request).with(hash_including(method: :get)).and_return(double(:response).as_null_object)
         transport.get('/example', mock: true)
       end
 
       it 'make post request' do
-        transport = EsClient::Transport.new('http://example.com', {})
+        transport = EsClient::Client.new('http://example.com', {})
         expect(transport.http).to receive(:request).with(hash_including(method: :post)).and_return(double(:response).as_null_object)
         transport.post('/example', mock: true)
       end
@@ -33,14 +33,14 @@ describe EsClient::Transport do
 
     context 'failure' do
       it 'reconnect on failed request' do
-        transport = EsClient::Transport.new('http://example.com', {})
+        transport = EsClient::Client.new('http://example.com', {})
         allow(transport.http).to receive(:request) { raise Excon::Errors::SocketError.new(StandardError.new) }
         expect(transport).to receive(:reconnect!)
         expect { transport.get('/example', mock: true) }.to raise_error
       end
 
       it 'retry failed request' do
-        transport = EsClient::Transport.new('http://example.com', {})
+        transport = EsClient::Client.new('http://example.com', {})
         allow(transport.http).to receive(:request) { raise Excon::Errors::SocketError.new(StandardError.new) }
         allow(transport).to receive(:reconnect!)
         expect(transport.http).to receive(:request).twice
--- spec/es_client_spec.rb
+++ spec/es_client_spec.rb
@@ -4,4 +4,12 @@ describe EsClient do
   it 'has a version number' do
     expect(EsClient::VERSION).not_to be nil
   end
+
+  it 'has http connection' do
+    expect(EsClient.client).to be_instance_of(::EsClient::Client)
+  end
+
+  it 'http connection persistent by default' do
+    expect(EsClient.client.http.data[:persistent]).to eq true
+  end
 end
