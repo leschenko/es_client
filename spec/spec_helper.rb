1d020e8d92a50dd87f96dbd4c2f677d5b2b97eb3
Alex Leschenko <leschenko.al@gmail.com>
2015-03-04 15:33:04 +0200

    Add byebug to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'es_client'
+require 'byebug'
 
 EsClient.setup do |config|
   config.log_path = File.expand_path('../../log/elasticsearch.log', __FILE__)
+  config.host = 'http://localhost:9201'
 end
