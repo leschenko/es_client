e20f424970cdf78d3f83b9793fabab782ff696a1
Alex Leschenko <leschenko.al@gmail.com>
2015-03-05 15:24:14 +0200

    Setup modules autoloading

--- lib/es_client.rb
+++ lib/es_client.rb
@@ -3,16 +3,22 @@ require 'excon'
 require 'active_support/all'
 
 module EsClient
-  mattr_accessor :logger, :logger_options, :host, :http_client_options
-
   autoload :Client, 'es_client/client'
   autoload :Response, 'es_client/response'
   autoload :Logger, 'es_client/logger'
   autoload :Index, 'es_client/index'
 
+  module ActiveRecord
+    autoload :Adapter, 'es_client/active_record/adapter'
+    autoload :Glue, 'es_client/active_record/glue'
+  end
+
+  mattr_accessor :callbacks_enabled, :logger, :logger_options, :host, :http_client_options
+
   @@host = 'http://localhost:9200'
   @@http_client_options = {persistent: true}
   @@logger_options = {log_binary: true, log_response: true, pretty: true}
+  @@callbacks_enabled = true
 
   def self.log_path=(path)
     self.logger = ::EsClient::Logger.new(path, logger_options)
